8c1cdd63c3e366082005a52e6116871f3332a49f
jungsunbeen <136215148+jungsunbeen@users.noreply.github.com>
2024-04-30 02:41:03 +0900

    del : 주석

--- week6/DOM/DOM.js
+++ week6/DOM/DOM.js
@@ -2,7 +2,7 @@ class DOM{
     constructor(tagName,innerText,className){
         this.node = document.createElement(tagName);
         this.node.innerText = innerText;
-        if (className) this.node.classList.add(className); //추가해주는거
+        if (className) this.node.classList.add(className);
     }
 }
 
