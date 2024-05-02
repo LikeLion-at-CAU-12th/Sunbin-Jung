110f7709f16340f730e828289bf8df5b5a90dfb9
jungsunbeen <136215148+jungsunbeen@users.noreply.github.com>
2024-05-02 22:48:25 +0900

    🎉 week 7 시작

---
+++ week7/prac1/filter.js
@@ -0,0 +1,24 @@
+const filterData = () => {
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    fetch(url)
+    .then((response)=>{
+        return response.json(); //json으로 변환한 거
+    })
+    .then((response)=>{
+        console.log(response.frontend);
+        const datas = response.frontend;
+
+        datas
+        .filter((data)=>data.role == '아기사자')
+        .map((data)=>{
+            const list = document.createElement('div');
+            list.innerHTML = `제 이름은 ${data.name}입니다.
+            저는 ${data.role}입니다. 그리고 제 전공은 ${data.major}입니다~!!`;
+
+            container.appendChild(list);
+            console.log(data);
+        })
+    })
+}
