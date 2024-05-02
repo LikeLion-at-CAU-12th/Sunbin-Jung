26481a9896de21bb2290cef7909e579d0e1bee74
정선빈 <136215148+jungsunbeen@users.noreply.github.com>
2024-05-03 00:09:36 +0900

    Revert "week 6 과제"

--- week6/DOM/Image.js
+++
@@ -1,9 +0,0 @@
-import DOM from "./DOM.js";
-
-class Image extends DOM{
-    constructor (className){
-        super('image','',className); //태그 이름, 텍스트 내용, 클래스 이름
-    }
-}
-
-export default Image;
--- week6/app.js
+++
@@ -1,9 +0,0 @@
-import TodoController from "./controller/TodoController.js";
-
-const addBtn = document.getElementById("input");
-const input = document.querySelector('input');
-
-addBtn.addEventListener('click',() => {
-    const todoController = new TodoController(input.value);
-    todoController.addTodo();
-}); //fuction(){} 는 ()=>{}랑 동일함
