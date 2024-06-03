83c0ed88753593911afae1eea34c88a17ef3305d
jungsunbeen <136215148+jungsunbeen@users.noreply.github.com>
2024-06-04 03:40:35 +0900

    색상 구현 완 && 파트 불러오기 완

--- week11/src/components/user/UserFilter.js
+++ week11/src/components/user/UserFilter.js
@@ -1,60 +1,70 @@
-import React from 'react'
-import styled from 'styled-components'
-import { filterType } from '../../constants/filtertype'
-import { getGenderUser, getPerPage } from '../../apis/userlist'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { filterType } from '../../constants/filtertype';
+import { getGenderUser, getPerPage, getPartUser } from '../../apis/userlist';
 
-const UserFilter = ({setFilter, setUserData, setCurPage}) => {
-    const handleClick = async(type, param) => {
-        if(type === "all"){
-            const response = await getPerPage(1);
-            //response값을 저장하기 위해서 새로운 상태(state)가 필요하다!
-            //useState를 이용해서 이 값을 저장해주도록 합시다~~
-            setUserData(response);
-            // console.log(response);
-            setCurPage(1);
-        } else if (type === "gender"){
-            const response = await getGenderUser(param);
-            setUserData(response);
-            // console.log(response);
-            setCurPage(1);
-        }
-        setFilter(param); //다른 값으로도 변경 가능~ 
+const UserFilter = ({ setFilter, setUserData, setCurPage }) => {
+  const [activeFilter, setActiveFilter] = useState('');
+
+  const handleClick = async (type, param) => {
+    if (type === "all") {
+      const response = await getPerPage(1);
+      setUserData(response);
+      setCurPage(1);
+    } else if (type === "gender") {
+      const response = await getGenderUser(param);
+      setUserData(response);
+      setCurPage(1);
+    } else if (type === "part") {
+      const response = await getPartUser(param);
+      setUserData(response);
+      setCurPage(1);
     }
+    setFilter(param);
+    setActiveFilter(param); // Update the active filter
+  }
+
   return (
-    <FilterLayout>{filterType.map(
-        (data, idx) => 
+    <FilterLayout>
+      {filterType.map((data, idx) => 
         <FilterBox
-        key={idx}
-        onClick={() => handleClick(data.type, data.param)}>{data.title}</FilterBox>
-    )}</FilterLayout>
-  )
+          key={idx}
+          $active={activeFilter === data.param}
+          onClick={() => handleClick(data.type, data.param)}
+        >
+          {data.title}
+        </FilterBox>
+      )}
+    </FilterLayout>
+  );
 }
 
-export default UserFilter
+export default UserFilter;
 
 const FilterLayout = styled.div`
-    display: flex;
-    width: 90%;
-    justify-content: space-between;
-    overflow-x: scroll;
-    padding-left: 2rem;
-    padding-right: 2rem;
-    margin-top: 2rem;
-    gap: 2rem;
-    &::-webkit-scrollbar{
-        display: none;
-    }
-`
+  display: flex;
+  width: 90%;
+  justify-content: space-between;
+  overflow-x: scroll;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  margin-top: 2rem;
+  gap: 2rem;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
 
 const FilterBox = styled.div`
-    display: flex;
-    padding: 1rem 4rem 1rem 4rem;
-    background-color: "#C9C9C9";
-    border-radius: 1rem;
-    font-size: 3rem;
-    white-space: nowrap;
-    &:hover{
-        cursor: pointer;
-        color: white;
-    }
-`
+  display: flex;
+  padding: 1rem 4rem;
+  background-color: ${(props) => props.$active ? "#FFFFFF" : "orange"};
+  border-radius: 1rem;
+  font-size: 3rem;
+  white-space: nowrap;
+  &:hover {
+    cursor: pointer;
+    /* color: white; */
+  }
+  color: ${(props) => props.$active ? "black" : "white"};
+`;
