0059015224f3082c6bec1d47be271529524648c1
jungsunbeen <tjqls041103@gmail.com>
2024-07-02 02:03:25 +0900

    modal 구현 완 디자인해야됨

--- week13/src/component/layout/Modal.js
+++ week13/src/component/layout/Modal.js
@@ -13,6 +13,7 @@ const Modal = ({ isOpen, onClose, onConfirm }) => {
   if (!isOpen) return null;
 
   return (
+    <ModalOverlay>
       <ModalContent mode={mode.modal}>
         <h3>{`${userName} ${email} ${item}`}</h3>
         <h2>입력한 정보가 맞습니까?</h2>
@@ -21,25 +22,27 @@ const Modal = ({ isOpen, onClose, onConfirm }) => {
           <button onClick={onClose}>취소</button>
         </Buttons>
       </ModalContent>
+    </ModalOverlay>
   );
 };
 
 export default Modal;
 
-// const ModalOverlay = styled.div`
-//   position: fixed;
-//   top: 0;
-//   left: 0;
-//   right: 0;
-//   bottom: 0;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   background: rgba(0, 0, 0, 0.5);
-// `;
+const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width:50%;
+  heigh:50%;
+  background: rgba(0, 0, 0.5);
+`;
 
 const ModalContent = styled.div`
-  background: ${({ mode }) => mode.background};
   padding: 2rem;
   border-radius: 8px;
 `;
