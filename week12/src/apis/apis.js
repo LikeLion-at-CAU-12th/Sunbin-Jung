08f90f32d217f823cb8b984a9a7de49a7b0a6ef1
jungsunbeen <tjqls041103@gmail.com>
2024-07-08 19:32:52 +0900

    rename : week14 초기 세팅

---
+++ week12/src/apis/apis.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+export const baseURL = "https://gominzipsession.o-r.kr";
+
+export const getQuestion = async () => {
+    try {
+        const response = await axios.get(`${baseURL}/liontest/question`);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching questions:", error);
+        throw error;
+    }
+}
+
+export const postResult = async (data) => {
+    try {
+        const response = await axios.post(`${baseURL}/liontest/result`, data);
+        return response.data;
+    } catch (error) {
+        console.error("Error posting result:", error);
+        throw error;
+    }
+}
+
+export const getResult = async (correctCount) => {
+    try {
+        const response = await axios.get(`${baseURL}/liontest/result/${correctCount}`);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching result:", error);
+        throw error;
+    }
+}
---
+++ week12/src/apis/user.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { getAuthAxios } from "./authAxios";
+
+// const baseURL = `http://yangzzago.kro.kr:3000`;
+
+// export const signUp = async(id,pw,name,age) => {
+//     const result = await axios.post(`${baseURL}/signup`,{
+//         id,
+//         pw,
+//         name,
+//         age,
+//     });
+//     return result;
+// };
+
+// export const login = async(id,pw) => {
+//     const result = await axios.post(`${baseURL}/login`, {
+//         id,
+//         pw,
+//     });
+//     console.log(result.data);
+//     return result.data;
+// };
+
+export const getMyPage = async() => {
+    const authAxios = getAuthAxios(localStorage.getItem("access"));
+    const result = authAxios.get("/mypage");
+    return result.data;
+};
+
+export const getNewRefreshToken = async () => {
+    try {
+        const accessToken = localStorage.getItem("access");
+        const refreshtoken = localStorage.getItem("refresh");
+
+        const result = await axios.post(
+            `${baseURL}/refresh`,
+            {
+                refreshtoken,
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            }
+        );
+        return result.data;
+    }catch (error) {
+        alert("토큰이 만료되었습니다. 다시 로그인해주세요.");
+    }
+}
